3b844e97cd27361530ac7f8ef3aa48ef279b2c12
greciaveronica <greciaveronica@gmail.com>
2016-09-28 16:22:08 -0500

    set options input in pageSizeSelector component

--- src/Unosquare.Tubular2.Web/wwwroot/tubular/page-size-selector.component.ts
+++ src/Unosquare.Tubular2.Web/wwwroot/tubular/page-size-selector.component.ts
@@ -11,17 +11,23 @@ import 'rxjs/add/operator/debounceTime';
     template: ` <form class="form-inline">
                 <div [ngClass]="form-group">
                 <label class="small">Page size</label>&nbsp;
-                <select (change)="onChange($event.target.value)" class="form-control input-sm ">
-                    <option *ngFor="let obj of options" [value]="obj">{{obj}}</option>
+                <select (change)="onChange($event.target.value)" class="form-control input-sm " >
+                    <option *ngFor="let obj of _options" [value]="obj">{{obj}}</option>
                 </select>
                 </div>
                 </form>`
 })
 export class PageSizeSelector {
-    options = [10, 20, 50, 100];
+    _options: any[] = [10, 20, 50, 100];
 
     constructor(private tbGrid: TubularGrid) {
     }
+
+    @Input('options')
+    set in(options: any[]) {
+        options != undefined ? this._options = options : this._options = [10, 20, 50, 100];
+    }
+
 
     onChange(newVal) {
         this.tbGrid._pageSize.next(newVal);
