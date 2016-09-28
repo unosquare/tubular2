420cfaa862d351719d02ecaa53ba1254c12d7fa4
H. Edmundo Ramírez <edmundormz@gmail.com>
2016-09-28 12:11:32 -0500

    Search Function

--- src/Unosquare.Tubular2.Web/wwwroot/tubular/grid-search.component.ts
+++ src/Unosquare.Tubular2.Web/wwwroot/tubular/grid-search.component.ts
@@ -7,15 +7,13 @@ import { TubularGrid }           from './grid.component';
     template: `<div>
                     <div class="input-group input-group-sm">
                     <span class="input-group-addon"><i class="fa fa-search"></i></span>
-                        <input type="text" class="form-control" 
+                        <input #toSearch type="text" class="form-control" 
                         [ngModel]="search"
                         (ngModelChange)="setSearch($event)"
                         placeholder="search . . ."  
                         />
-                        <span class="input-group-btn" 
-                        ng-show="">
-                            <button class="btn btn-default" uib-tooltip="" 
-                            ng-click="">
+                        <span class="input-group-btn" >
+                            <button class="btn btn-default" (click)="clearInput()">
                             <i class="fa fa-times-circle"></i>
                             </button>
                         </span>
@@ -28,6 +26,11 @@ export class GridSearch {
     constructor(private tbGrid: TubularGrid) { }
 
     // TODO: Restore value from localstorage?
+
+    clearInput() {
+        this.tbGrid.freeTextSearch.next("");
+        this.search = "";
+    }
 
     setSearch(event: any) {
         this.tbGrid.freeTextSearch.next(event);
