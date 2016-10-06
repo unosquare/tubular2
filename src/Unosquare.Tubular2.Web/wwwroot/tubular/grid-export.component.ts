2ddf1421bb0cf64d4da904db7c3bdd9b49b3ea55
H. Edmundo Ramírez <edmundormz@gmail.com>
2016-10-06 14:15:34 -0500

    UI changes

--- src/Unosquare.Tubular2.Web/wwwroot/tubular/grid-export.component.ts
+++ src/Unosquare.Tubular2.Web/wwwroot/tubular/grid-export.component.ts
@@ -4,10 +4,10 @@ import { TubularGrid } from './grid.component';
 @Component({
     selector: 'grid-export',
     template: `<div ngbDropdown class="d-inline-block">
-                <button ngbDropdownToggle class="btn btn-sm btn-outline-primary" id="dropdownExport">
+                <button ngbDropdownToggle class="btn btn-outline-primary btn-sm">
                 <span class="fa fa-download"></span>&nbsp;EXPORT CSV&nbsp;<span class="caret"></span>
                </button>
-               <div class="dropdown-menu" aria-labelledby="dropdownExport">
+               <div class="dropdown-menu">
                 <button class="dropdown-item" (click)="downloadCsv()">Current rows</button>
                 <button class="dropdown-item" (click)="downloadAllCsv()">All rows</button>
                </div>
