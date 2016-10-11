256723c904e46862c3c92b20ced841d907e7d1c3
H. Edmundo Ramírez <edmundormz@gmail.com>
2016-10-11 16:31:44 -0500

    ngbPagination implemented

--- src/Unosquare.Tubular2.Web/wwwroot/tubular/grid-pager.component.ts
+++ src/Unosquare.Tubular2.Web/wwwroot/tubular/grid-pager.component.ts
@@ -4,38 +4,29 @@ import { TubularGrid, GridPageInfo }      from './grid.component';
 
 @Component({
     selector: 'grid-pager',
-    template: `
-    <div class="btn-group">
-        <button (click)="goTo(0)" class="btn btn-primary"
-            [disabled]="info.currentPage == 0">
-            <i class="fa fa-fast-backward"></i>
-        </button>
-        <button *ngFor="let page of pages" [hidden]="page < 0"
-            (click)="goTo(page)" class="btn btn-secondary"
-            [ngClass]="{active: page == info.currentPage - 1}">
-            {{page + 1}}
-        </button>
-        <button (click)="goTo(info.totalPages)" class="btn btn-primary"
-            [disabled]="currentPage == (info.totalPages-1)">
-            <i class="fa fa-fast-forward"></i>
-        </button>
+    template: 
+    `<div>
+    <ngb-pagination 
+    [collectionSize]="info.filteredRecordCount"
+    [(page)]="info.currentPage"
+    [boundaryLinks]="true"
+    (pageChange)="goTo($event)"
+    ></ngb-pagination>
     </div>`
 })
 export class GridPager {
     info = new GridPageInfo();
-    pages: number[];
 
     constructor(private tbGrid: TubularGrid) { }
 
     ngOnInit() {
         this.tbGrid.pageInfo.subscribe((x: GridPageInfo) => {
             this.info = x;
-            this.pages = Array(x.totalPages).fill(0).map((x, i) => i); 
         });
     }
 
     goTo(page: number) {
         this.info.currentPage = page;
-        this.tbGrid.page.next(page);
+        this.tbGrid.page.next(page-1);
     }
 }
