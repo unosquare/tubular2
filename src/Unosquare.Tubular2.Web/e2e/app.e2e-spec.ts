5ada1a7a4a12262f9ec5c584258c8930b368aab3
greciaveronica <greciaveronica@gmail.com>
2016-10-26 14:36:19 -0500

    Add sorting e2e test

--- src/Unosquare.Tubular2.Web/e2e/app.e2e-spec.ts
+++ src/Unosquare.Tubular2.Web/e2e/app.e2e-spec.ts
@@ -1,22 +1,18 @@
-﻿	///<reference path="../node_modules/@types/jasmine/index.d.ts"/>
+﻿///<reference path="../node_modules/@types/jasmine/index.d.ts"/>
 
-// import { browser, element, by } from '../node_modules/protractor/built';
+import { browser, element, by } from '../node_modules/protractor/built';
 
-// describe('QuickStart E2E Tests', function () {
+describe('QuickStart E2E Tests', function () {
 
-//    let expectedMsg = 'My First Angular App';
+    let expectedMsg = 'Tubular2';
 
 
-//    beforeEach(function () {
-//        browser.get('');
-//    });
+    beforeEach(function () {
+        browser.get('');
+    });
 
-//    it('should display: ' + expectedMsg, function () {
-//        expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
-//    });
+    it('should display: ' + expectedMsg, function () {
+        expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+    });
 
-// });
-
-describe('1st tests', function () {
-    it('true is true', function () { return expect(true).toBe(true); });
 });
---
+++ src/Unosquare.Tubular2.Web/e2e/sorting.e2e-spec.ts
@@ -0,0 +1,45 @@
+﻿import { browser, element, by } from '../node_modules/protractor/built';
+
+describe('grid sorting', () => {
+
+    let dataSetLowerId = '1',
+        dataSetHigherId = '53',
+        dataSetLowerCustomerName = 'Microsoft',
+        dataSetHigherCustomerName = 'Microsoft',
+        dataSetLowerDate = '2016-08-19',
+        dataSetHigherDate = '2016-02-01';
+
+    let paginator = element(by.tagName('ngb-pagination')).$$('nav').$$('ul'),
+        orderIdSorting = element(by.className('column-header')).$$('span').first(),
+        firstDataRow = element(by.tagName('tbody')).$$('tr').first();
+    
+    beforeEach(() => {
+    });
+
+    it('should order data in ascending order when click-sorting an unsorted numeric column', () => {
+        orderIdSorting.click();
+        expect(firstDataRow.$$('td').get(1).getText()).toEqual(dataSetLowerId);
+    });
+
+    //it('', () => {
+
+    //});
+
+    //it('', () => {
+
+    //});
+
+    //it('', () => {
+
+    //});
+
+    //it('', () => {
+
+    //});
+
+    //it('', () => {
+
+    //});
+
+
+});
