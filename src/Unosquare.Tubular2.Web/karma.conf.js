9c10fd64fa8e07dd1912be0bbb75d963c8b3d29c
Geovanni Perez <geovanni.perez@gmail.com>
2016-10-26 14:48:32 -0500

    Update karma.conf.js

--- src/Unosquare.Tubular2.Web/karma.conf.js
+++ src/Unosquare.Tubular2.Web/karma.conf.js
@@ -97,7 +97,7 @@ module.exports = function (config) {
         singleRun: false
     };
     
-    if(process.env.TRAVIS) configuration.browsers = ['Chrome_travis_ci'];
+    if(process.env.TRAVIS) configuration.browsers = ['Firefox'];
     
     config.set(configuration);
 }
