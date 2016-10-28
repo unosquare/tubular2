ff5a019d9cf2eb21ea49dc31488f2c3e4a386ef3
greciaveronica <greciaveronica@gmail.com>
2016-10-28 11:52:42 -0500

    Add grid pager info test

---
+++ src/Unosquare.Tubular2.Web/e2e/gridPagerInfo.e2e-spec.ts
@@ -0,0 +1,29 @@
+﻿import { browser, element, by } from '../node_modules/protractor/built';
+
+describe('grid pager info', () => {
+
+    let paginator = element(by.tagName('ngb-pagination')).$$('nav').$$('ul').$$('li'),
+        gridPagerInfo = element(by.tagName('grid-pager-info')).$$('div'),
+        pageSizeSelector = element(by.tagName('page-size-selector')).$$('select'),
+        gridSearchInput = element(by.tagName('grid-search')).$$('div').$$('div').$$('input');
+
+
+    beforeEach(() => {
+        browser.refresh();
+    });
+
+    it('should show text in accordance to numbered of filter rows and current results-page',() => {
+        expect(gridPagerInfo.first().getText()).toEqual('Showing 1 to 10 of 53 records');
+        paginator.get(7).$$('a').click();
+        pageSizeSelector.$$('option').get(1).click();
+        expect(gridPagerInfo.first().getText()).toEqual('Showing 21 to 40 of 53 records');
+        paginator.get(5).$$('a').click();
+        expect(gridPagerInfo.first().getText()).toEqual('Showing 41 to 60 of 53 records');
+    });
+
+    it('should show count in footer', () => {
+        gridSearchInput.sendKeys('a');
+        expect(gridPagerInfo.first().getText()).toContain('(Filtered from 53 total records)');
+    });
+
+});
