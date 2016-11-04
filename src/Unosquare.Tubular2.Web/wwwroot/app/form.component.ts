66e02e3ea68ed2723547f91e821d709bc6a6ce93
greciaveronica <greciaveronica@gmail.com>
2016-11-04 15:34:38 -0600

    Add observables to manipulate from data

--- src/Unosquare.Tubular2.Web/wwwroot/app/form.component.ts
+++ src/Unosquare.Tubular2.Web/wwwroot/app/form.component.ts
@@ -1,6 +1,8 @@
-﻿import { Component, Output, EventEmitter, } from '@angular/core';
-import { Router, ActivatedRoute, Params } from '@angular/router';
+﻿import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { FormGroup, FormBuilder } from '@angular/forms';
+  
+import { BehaviorSubject }  from 'rxjs/BehaviorSubject';
 
 import { TubularDataService } from '@tubular2/tubular2';
 
@@ -12,12 +14,16 @@ export class FormComponent {
 
     request: any;
     detailsForm: FormGroup;
-    @Output() dataRequest = new EventEmitter<any>();
+    private _row = new BehaviorSubject(this.emptyRow());
+    row = this._row.asObservable();
 
     constructor(private route: ActivatedRoute, public formBuilder: FormBuilder, private dataService: TubularDataService) { }
 
     ngOnInit() {
         let id: number;
+        this.row.subscribe(() => {
+            this.formBuild();
+        });
 
         this.route.params.forEach((params: Params) => {
             id = params['id'];
@@ -25,22 +31,25 @@ export class FormComponent {
         
         if (id != undefined) {
             this.getRow(id);
-        } else {
-            this.emptyRow();
         }
     }
 
     getRow(id: number): void {
         this.dataService.getData("http://tubular.azurewebsites.net/api/orders/" + id).subscribe(
-            data => { this.dataRequest.emit(data); }
+            data => { this._row.next(data); }
             );
     }
 
-    emptyRow(): void {
-        this.detailsForm = this.formBuilder.group({
+    formBuild(): void {
+        this.detailsForm = this.formBuilder.group(this._row.value);
+    }
+
+
+    emptyRow(): any {
+        return {
             CustomerName: "",
             ShippedDate: Date,
             ShipperCity: ""
-        });
+        };
     }
 }
