d7cd386189d06b7745035c2cc99230cef9f11b4b
Geo Perez <geovanni.perez@gmail.com>
2016-11-09 15:36:53 -0600

    Login Component separated view from controller

--- src/Unosquare.Tubular2.Web/wwwroot/app/login.component.ts
+++ src/Unosquare.Tubular2.Web/wwwroot/app/login.component.ts
@@ -1,27 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { TubularDataService } from '@tubular2/tubular2';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 @Component({
     selector: 'login',
-    template: `<form [formGroup]="loginForm" (ngSubmit)="onSubmit(loginForm)">
-                    <div class="row">
-                        <div class="col-md-4 col-md-push-4">
-                            <div class="form-group">
-                                <label>User:</label>
-                                <input class="form-control" type="text" formControlName="username"/>
-                                <label>Password:</label>
-                                <input class="form-control" type="password" formControlName="password"/>
-                             </div>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-sm btn-success btn-block" [disabled]="loginForm.invalid">Login</button>
-                            </div>
-                            <span class="form-group" *ngIf=""></span>
-                        </div>
-                    </div>
-              </form>`
+    templateUrl: '/app/login.component.html'
 })
 
 export class LoginComponent implements OnInit {
@@ -39,13 +25,13 @@ export class LoginComponent implements OnInit {
     onSubmit(data) {
         let username = data.value.username;
         let password = data.value.password;
-        this.dataService.setTokenUrl('http://tubular.azurewebsites.net/token')
+        this.dataService.setTokenUrl('http://tubular.azurewebsites.net/token');
+        
         this.dataService.authenticate(username, password,
-            () => {
-                this.router.navigate(['']);
-            }, (error) => {
+            () => this.router.navigate(['']), 
+            (error) => {
                 this.toastr.error(error.status + ' - ' + error.errorBody.error_description, 'Filed Login');
                 this.router.navigate(['/login']);
-        }, true);      
+        });      
     }
 }
