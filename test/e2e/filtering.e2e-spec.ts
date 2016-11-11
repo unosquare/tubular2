ecadd0e9b3b1a4b6fff6456669525169f7234b91
greciaveronica <greciaveronica@gmail.com>
2016-11-11 14:25:04 -0600

    Add date column filter add, add text search test

--- test/e2e/filtering.e2e-spec.ts
+++ test/e2e/filtering.e2e-spec.ts
@@ -2,19 +2,22 @@ import { browser, element, by } from '../../node_modules/protractor/built';
 
 describe('Filtering e2e Tests', () => {
 
-    beforeAll(() => {
-        browser.get('/');
-    });
+    let columnHeaders;
 
     function setPagination() {
         element(by.tagName('page-size-selector')).$('form').$('div').$('select').$('[value="50"]').click();
         element(by.tagName('grid-pager')).$('ngb-pagination').$('nav').$('ul').$$('li').get(2).$('a').click();
     }
 
-    describe('Column filters', () => {
+    beforeAll(() => {
+        browser.get('/');
+        setPagination();
+        columnHeaders = element(by.tagName('thead')).$$('tr').first().$$('th');
+    });
 
-        let columnHeaders,
-            columnFilter,
+    describe('Column filters', () => {
+    
+        let columnFilter,
             filterBtn,
             popOverForm,
             applyBtn,
@@ -23,8 +26,6 @@ describe('Filtering e2e Tests', () => {
             valueInput;
 
         beforeAll(() => {
-            setPagination();
-            columnHeaders = element(by.tagName('thead')).$$('tr').first().$$('th');
             columnFilter = columnHeaders.get(2).$('.column-header').$('div');
             filterBtn = columnFilter.$('button');
         });
@@ -44,12 +45,12 @@ describe('Filtering e2e Tests', () => {
             setPagination();
         });
 
-        it('should disable Value text-input for "None" filter', () => {
+        it('should disable Value text-input for "None" filter', () => {  
             filterSelect.$('[value="None"]').click();
             expect(valueInput.getAttribute('disabled')).toBe('true');
-            clearBtn.click();
-        });
-
+            clearBtn.click();                            
+        });       
+                
         it('should decorate popover button when showing data is being filtered for its column', () => {
             expect(filterBtn.getAttribute('class')).not.toMatch(/btn-success/);
             filterSelect.$('[value="Equals"]').click();
@@ -57,8 +58,8 @@ describe('Filtering e2e Tests', () => {
             applyBtn.click();
             expect(filterBtn.getAttribute('class')).toMatch(/btn-success/);
         });
-
-        //it('should correctly filter data for the "Equals" filtering option', () => { TODO: review, is not passing
+        
+        //it('should correctly filter data for the "Equals" filtering option', () => { NO PASA no esta filtrando
         //    let filteredCustomer = 'Microsoft';
         //    filterSelect.$('[value="Equals"]').click();
         //    valueInput.sendKeys(filteredCustomer);
@@ -69,7 +70,7 @@ describe('Filtering e2e Tests', () => {
         //    });
         //});
 
-        //it('should correctly filter data for the "Not Equals" filtering option', () => {  TODO: review, is not passing
+        //it('should correctly filter data for the "Not Equals" filtering option', () => { NO PASA no esta filtrando
         //    let filterOk = true;
         //    let notShowingCustomer = 'Microsoft';
         //    filterSelect.$('[value="NotEquals"]').click();
@@ -83,58 +84,304 @@ describe('Filtering e2e Tests', () => {
 
         it('should correctly filter data for the "Contains" filtering option', () => {
             let containedString = 'La';
+            let filterOk = true;
+            
             filterSelect.$('[value="Contains"]').click();
             valueInput.sendKeys(containedString);
             applyBtn.click();
             let dataRows = element(by.tagName('tbody')).$$('tr');
             dataRows.each((row) => {
-                expect(row.$$('td').get(2).getText()).toContain(containedString);
-            });
+                row.$$('td').get(2).getText().then((filter) => {
+                   filterOk = filterOk && (filter.indexOf(containedString) !== -1);
+                })
+            }).then(() => {
+                expect(filterOk).toBe(true);  
+            });            
+        });
+
+        it('should correctly filter data for the "Not Contains" filtering option', () => {
+            let notContainedString = 'La';
+            let filterOk = true;
+            
+            filterSelect.$('[value="NotContains"]').click();
+            valueInput.sendKeys(notContainedString);
+            applyBtn.click();
+            let dataRows = element(by.tagName('tbody')).$$('tr');
+            dataRows.each((row) => {
+                row.$$('td').get(2).getText().then((filter) => {
+                   filterOk = filterOk && (filter.indexOf(notContainedString) === -1);
+                })
+            }).then(() => {
+                expect(filterOk).toBe(true);  
+            });            
         });
 
         it('should correctly filter data for the "Starts With" filtering option', () => {
             let startsWithString = 'Uno';
+            let filterOk = true;
+
             filterSelect.$('[value="StartsWith"]').click();
             valueInput.sendKeys(startsWithString);
             applyBtn.click();
             let dataRows = element(by.tagName('tbody')).$$('tr');
             dataRows.each((row) => {
-                expect(row.$$('td').get(2).getText()).toMatch(/^Uno/i);
+                row.$$('td').get(2).getText().then((filter) => {
+                   filterOk = filterOk && (filter.indexOf(startsWithString) === 0);
+                })
+            }).then(() => {
+                expect(filterOk).toBe(true);  
             });
         });
 
         it('should correctly filter data for the "Not Starts With" filtering option', () => {
             let notStartsWithString = 'Uno';
+            let filterOk = true;
+
             filterSelect.$('[value="NotStartsWith"]').click();
             valueInput.sendKeys(notStartsWithString);
             applyBtn.click();
             let dataRows = element(by.tagName('tbody')).$$('tr');
             dataRows.each((row) => {
-                expect(row.$$('td').get(2).getText()).not.toMatch(/^Uno/i);
+                row.$$('td').get(2).getText().then((filter) => {
+                   filterOk = filterOk && (filter.indexOf(notStartsWithString) !== 0);
+                })
+            }).then(() => {
+                expect(filterOk).toBe(true);  
             });
         });
 
         it('should correctly filter data for the "Ends With" filtering option', () => {
             let endsWithString = 'xo';
+            let filterOk = true;
+
             filterSelect.$('[value="EndsWith"]').click();
             valueInput.sendKeys(endsWithString);
             applyBtn.click();
             let dataRows = element(by.tagName('tbody')).$$('tr');
             dataRows.each((row) => {
-                expect(row.$$('td').get(2).getText()).toMatch(/xo$/);
+                row.$$('td').get(2).getText().then((filter) => {
+                   filterOk = filterOk && (filter.indexOf(endsWithString) === (filter.length - endsWithString.length));
+                })
+            }).then(() => {
+                expect(filterOk).toBe(true);  
             });
         });
 
         it('should correctly filter data for the "Not Ends With" filtering option', () => {
             let endsWithString = 'xo';
+            let filterOk = true;
+
             filterSelect.$('[value="NotEndsWith"]').click();
             valueInput.sendKeys(endsWithString);
             applyBtn.click();
             let dataRows = element(by.tagName('tbody')).$$('tr');
             dataRows.each((row) => {
-                expect(row.$$('td').get(2).getText()).not.toMatch(/xo$/);
+                row.$$('td').get(2).getText().then((filter) => {
+                   filterOk = filterOk && (filter.indexOf(endsWithString) !== (filter.length - endsWithString.length));
+                })
+            }).then(() => {
+                expect(filterOk).toBe(true);  
             });
         });
 
     });
+
+    describe('DateTime column filter', () => {
+
+       let columnFilter,
+           filterBtn,
+           popOverForm,
+           applyBtn,
+           clearBtn,
+           filterSelect,
+           valueInput;
+
+       beforeAll(() => {
+           columnFilter = columnHeaders.get(4).$('.column-header').$('div');
+           filterBtn = columnFilter.$('button');
+       });
+
+       beforeEach(() => {
+           filterBtn.click();
+           popOverForm = columnFilter.$('ngb-popover-window').$('.popover-content').$('filter-dialog').$('form');
+           applyBtn = popOverForm.$('.row').$$('div').get(0).$('.btn-success');
+           clearBtn = popOverForm.$('.row').$$('div').get(1).$('.btn-danger');
+           filterSelect = popOverForm.$$('.form-group').get(0).$('select');
+           valueInput = popOverForm.$$('.form-group').get(1).$$('input').first();
+       });
+
+       afterEach(() => {
+           filterBtn.click();
+           clearBtn.click();
+           setPagination();
+       });
+
+       it('should disable Value text-input for "None" filter', () => {
+           filterSelect.$('[value="None"]').click();
+           expect(valueInput.getAttribute('disabled')).toBe('true');
+           clearBtn.click();
+       });
+
+       it('should decorate popover button when showing data is being filtered for its column', () => {
+           expect(filterBtn.getAttribute('class')).not.toMatch(/btn-success/);
+           filterSelect.$('[value="Gt"]').click();
+           valueInput.sendKeys('02/05/2016');
+           applyBtn.click();
+           expect(filterBtn.getAttribute('class')).toMatch(/btn-success/);
+       });
+
+
+       //it('should correctly filter data for the "Equals" filtering option', () => {
+       //    let filteredDate = '01/30/2016';
+       //    filterSelect.$('[value="Equals"]').click();
+       //    valueInput.sendKeys(filteredDate);
+       //    applyBtn.click();
+       //    let dataRows = element(by.tagName('tbody')).$$('tr');
+       //    dataRows.each((row) => {
+       //        expect(row.$$('td').get(3).getText()).toEqual(filteredDate);
+       //    });
+       //});
+
+
+       //it('should correctly filter data for the "Not Equals" filtering option', () => {
+       //    let filteredDate = '01/30/2016';
+       //    filterSelect.$('[value="Equals"]').click();
+       //    valueInput.sendKeys(filteredDate);
+       //    applyBtn.click();
+       //    let dataRows = element(by.tagName('tbody')).$$('tr');
+       //    dataRows.each((row) => {
+       //        expect(row.$$('td').get(3).getText()).not.toEqual(filteredDate);
+       //    });
+       //});
+
+
+       it('should correctly filter data for the "Between" filtering option', () => {
+           let filterOk = true;
+           let minDate = new Date('12/29/2015 00:00 AM');
+           let maxDate = new Date('12/31/2015 00:00 AM');
+           let argumentInput = popOverForm.$$('.form-group').get(1).$$('input').last();
+           filterSelect.$('[value="Between"]').click();
+           valueInput.sendKeys(minDate);
+           argumentInput.sendKeys(maxDate);
+           applyBtn.click();
+           let dataRows = element(by.tagName('tbody')).$$('tr');
+           dataRows.each((row) => {
+                row.$$('td').get(4).getText().then((filter) => {
+                   filterOk = filterOk && ((minDate <= new Date(filter)) && (new Date(filter) <= maxDate));
+                })
+            }).then(() => {
+                expect(filterOk).toBe(true);  
+            });
+       });
+
+
+       //it('should correctly filter data for the "Greater-or-equal" filtering option', () => {
+       //});
+
+
+       //it('should corretlly filter data for the "Greater" filtering option', () => {
+       //});
+
+       //it('should correctly filter data for the "Less-or-equal" filtering option', () => {
+       //});
+
+       //it('should correctly filter data for the "Less" filtering option', () => {
+       //});
+
+       //it('', () => {
+       //});
+    });
+
+    describe('Text Search', () => {
+
+        let gridSearch,
+            searchInput,
+            clearBtn;
+
+        beforeAll(() => {
+            gridSearch = element(by.tagName('grid-search')).$('div').$('div');
+            searchInput = gridSearch.$('input');
+            clearBtn = gridSearch.$('button');
+        });
+
+        beforeEach(function () {
+            clearBtn.isDisplayed().then((displayed) => {
+                if (displayed) {
+                    clearBtn.click();
+                }
+            });
+        });
+
+        it('min-chars is not set', () => {
+            expect(searchInput.getAttribute('min-chars')).toBe(null);
+        });
+
+        it('should filter data in searchable-column customer name to matching inputted text, starting from 2 characters', () => {
+            let filteredCustomer = 'Microsoft';
+            let filterOk = true;
+
+            searchInput.sendKeys('c');
+            let dataRows = element(by.tagName('tbody')).$$('tr');
+            dataRows.each((row) => {
+                row.$$('td').get(2).getText().then((customerName) => {
+                   filterOk = filterOk && (customerName == filteredCustomer);
+                })
+            }).then(() => {
+                expect(filterOk).toBe(false);  
+            }).then(() => {
+                filterOk = true;
+                searchInput.sendKeys('r');
+                let dataRows = element(by.tagName('tbody')).$$('tr');
+                dataRows.each((row) => {
+                    row.$$('td').get(2).getText().then((customerName) => {
+                        filterOk = filterOk && (customerName == filteredCustomer);
+                    })
+                }).then(() => {
+                    expect(filterOk).toBe(true);
+                })
+            });
+        });
+
+        it('should filter data in searchable-column shipper city to matching inputted text, starting from 2 characters', () => {
+            let filteredCity = 'Los Angeles, CA, USA';
+            let filterOk = true;
+
+            searchInput.sendKeys('lo');
+            let dataRows = element(by.tagName('tbody')).$$('tr');
+            dataRows.each((row) => {
+                row.$$('td').get(2).getText().then((cityName) => {
+                   filterOk = filterOk && (cityName == filteredCity);
+                })
+            }).then(() => {
+                expect(filterOk).toBe(false);  
+            }).then(() => {
+                filterOk = true;
+                searchInput.sendKeys('r');
+                let dataRows = element(by.tagName('tbody')).$$('tr');
+                dataRows.each((row) => {
+                    row.$$('td').get(2).getText().then((cityName) => {
+                        filterOk = filterOk && (cityName == filteredCity);
+                    })
+                }).then(() => {
+                    expect(filterOk).toBe(true);
+                })
+            });
+        });
+
+        it('should show clear button when there is inputted text only', () => {
+            expect(clearBtn.isDisplayed()).toBe(false);
+            searchInput.sendKeys('1').then(() => {
+                expect(clearBtn.isDisplayed()).toBe(true);
+            });
+        });
+
+
+        
+        it('should clear text search when clicking clear button', () => {
+            searchInput.sendKeys('uno');
+            clearBtn.click();
+            expect(searchInput.getText()).toBe('');
+        });
+    });
+
 });
