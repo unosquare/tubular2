b939db078706b923eebb281790c47cb4a845397b
Geovanni Perez <geovanni.perez@gmail.com>
2017-02-09 10:45:54 -0600

    Update grid-pager-info.e2e-spec.ts

--- test/e2e/grid-pager-info.e2e-spec.ts
+++ test/e2e/grid-pager-info.e2e-spec.ts
@@ -13,16 +13,16 @@ describe('grid pager info', () => {
     });
 
     it('should show text in accordance to numbered of filter rows and current results-page',() => {
-        expect(gridPagerInfo.first().getText()).toEqual('Showing 1 to 10 of 49 records');
+        expect(gridPagerInfo.first().getText()).toEqual('Showing 1 to 10 of 500 records');
         paginator.get(7).$$('a').click();
         pageSizeSelector.$('[value="20"]').click();
-        expect(gridPagerInfo.first().getText()).toEqual('Showing 21 to 40 of 49 records');
+        expect(gridPagerInfo.first().getText()).toEqual('Showing 21 to 40 of 500 records');
         paginator.get(5).$$('a').click();
-        expect(gridPagerInfo.first().getText()).toEqual('Showing 41 to 60 of 49 records');
+        expect(gridPagerInfo.first().getText()).toEqual('Showing 61 to 80 of 500 records');
     });
 
     it('should show count in footer', () => {
         gridSearchInput.sendKeys('a');
-        expect(gridPagerInfo.first().getText()).toContain('(Filtered from 49 total records)');
+        expect(gridPagerInfo.first().getText()).toContain('Showing 1 to 10 of 466 records (Filtered from 500 total records)');
     });
 });
