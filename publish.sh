fa2b6525b7fb6995b8b5add34dbdf75fc857c587
Geo Perez <geovanni.perez@gmail.com>
2017-03-10 15:25:01 -0600

    Working on Reports publishing

---
+++ publish.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+TARGET_BRANCH="gh-pages"
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO out
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Move content
+mkdir out/reports/$TRAVIS_BUILD_NUMBER
+cd report
+cp -r * ../out/reports/$TRAVIS_BUILD_NUMBER
+cd ..
+
+# Now let's go have some fun with the cloned repo
+cd out
+
+git config credential.helper "store --file=.git/credentials"; echo "https://${GITHUBKEY}:@github.com" > .git/credentials 2>/dev/null
+git config user.name "Travis CI"
+git config user.email "geovanni.perez@gmail.com"
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add reports/*
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
