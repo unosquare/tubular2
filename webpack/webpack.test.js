2007071589cfc0df890560afa814ef8632c92bf1
Geo Perez <geovanni.perez@gmail.com>
2017-03-13 15:30:40 -0600

    trying to fix webpack

--- webpack/webpack.test.js
+++ webpack/webpack.test.js
@@ -6,20 +6,28 @@ const webpack = require('webpack');
 module.exports = {
     devtool: 'inline-source-map',
     module: {
-        preLoaders: [
-            //{ exclude: /node_modules/, loader: 'tslint', test: /\.ts$/ }
-        ],
         loaders: [
             { loader: 'raw', test: /\.(css|html)$/ },
             { exclude: /node_modules/, loader: 'ts-loader', test: /\.ts$/ }
+        ],
+        rules: [
+       {
+         test: /\.ts$/,
+         use: [
+           {
+             loader: 'tslint-loader',
+             options: {
+               configFile: 'tslint.json'
+             }
+           }
+         ],
+         exclude: [/\.(spec|e2e)\.ts$/]
+       },
         ]
     },
     resolve: {
-        extensions: ['', '.js'],
-        modulesDirectories: ['node_modules'],
-        root: path.resolve('.')
-    },
-    tslint: {
-        emitErrors: true
+        extensions: ['.js'],
+        //modulesDirectories: ['node_modules'],
+        //root: path.resolve('.')
     }
 };
