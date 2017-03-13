7f24309f10f131032aabeded0875abf44ba3ba44
Geo Perez <geovanni.perez@gmail.com>
2017-03-13 14:39:39 -0600

    Minor issues

--- sample/gulpfile.js
+++ sample/gulpfile.js
@@ -4,43 +4,39 @@ var del = require('del');
 var webpack = require('webpack');
 var webpackStream = require('webpack-stream');
 
-var paths = {
-    libs: {
-        '@angular/core': 'node_modules/@angular/core/bundles/core.umd.js',
-        '@angular/common': 'node_modules/@angular/common/bundles/common.umd.js',
-        '@angular/compiler': 'node_modules/@angular/compiler/bundles/compiler.umd.js',
-        '@angular/forms': 'node_modules/@angular/forms/bundles/forms.umd.js',
-        '@angular/http': 'node_modules/@angular/http/bundles/http.umd.js',
-        '@angular/platform-browser': 'node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
-        '@angular/platform-browser-dynamic': 'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-        '@angular/router': 'node_modules/@angular/router/bundles/router.umd.js',
-        'zone.js': 'node_modules/zone.js/**/*',
-        'rxjs': 'node_modules/rxjs/**/*',
-        'reflect': 'node_modules/reflect-metadata/*',
-        'systemjs': 'node_modules/systemjs/**/*',
-        'moment': 'node_modules/moment/*',
-        'filesaver': 'node_modules/filesaver.js/*.js',
-        'ng2-toastr': 'node_modules/ng2-toastr/ng2-toastr.*',
-        '@ng-bootstrap': 'node_modules/@ng-bootstrap/ng-bootstrap/**/*',
-        '@tubular2': '../lib/**/*'
-    }
+var libs = {
+    '@angular/core': 'node_modules/@angular/core/bundles/core.umd.js',
+    '@angular/common': 'node_modules/@angular/common/bundles/common.umd.js',
+    '@angular/compiler': 'node_modules/@angular/compiler/bundles/compiler.umd.js',
+    '@angular/forms': 'node_modules/@angular/forms/bundles/forms.umd.js',
+    '@angular/http': 'node_modules/@angular/http/bundles/http.umd.js',
+    '@angular/platform-browser': 'node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
+    '@angular/platform-browser-dynamic': 'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+    '@angular/router': 'node_modules/@angular/router/bundles/router.umd.js',
+    'zone.js': 'node_modules/zone.js/**/*',
+    'rxjs': 'node_modules/rxjs/**/*',
+    'reflect': 'node_modules/reflect-metadata/*',
+    'systemjs': 'node_modules/systemjs/**/*',
+    'moment': 'node_modules/moment/*',
+    'filesaver': 'node_modules/filesaver.js/*.js',
+    'ng2-toastr': 'node_modules/ng2-toastr/ng2-toastr.*',
+    '@ng-bootstrap': 'node_modules/@ng-bootstrap/ng-bootstrap/**/*',
+    '@tubular2': '../lib/**/*'
 };
 
-gulp.task('tubular2-module', function() {
-    return gulp.src(['../../package.json', '../lib/**/*.ts'])
+gulp.task('tubular2-module', () => {
+    return gulp.src(['../package.json', '../lib/**/*.ts'])
         .pipe(gulp.dest('node_modules/@tubular2/tubular2'));
 });
 
-gulp.task('lib', ['tubular2-module'], function() {
-    Object.keys(paths.libs).forEach(function(key) {
-        gulp.src(paths.libs[key]).pipe(gulp.dest('wwwroot/scripts/lib/' + key));
-    });
-});
+gulp.task('lib', ['tubular2-module'], () => {
+    Object.keys(libs).forEach(key => gulp.src(libs[key]).pipe(gulp.dest('scripts/lib/' + key)));
+})
 
-gulp.task('clean', () => del(['node_modules/@tubular2', 'wwwroot/scripts/**/*']));
+gulp.task('clean', () => del(['node_modules/@tubular2', 'scripts/**/*']));
 
-var standardBuild = function(watch) {
-    gulp.src('wwwroot/app/main.ts')
+var standardBuild = watch => {
+    gulp.src('app/main.ts')
         .pipe(webpackStream({
             devtool: 'source-map',
             output: { filename: '[name].js', },
@@ -54,7 +50,7 @@ var standardBuild = function(watch) {
             },
             watch: watch
         }))
-        .pipe(gulp.dest('wwwroot/dist/'));
+        .pipe(gulp.dest('dist/'));
 }
 
 gulp.task('build', ['lib'], () => standardBuild(false));
