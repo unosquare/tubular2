c53518774c4dd47a302c2384a30e679580b94f15
Israel-94 <arnulfo_israel@hotmail.com>
2017-04-04 14:17:59 -0500

    Changes to the spec of tubular.module

--- lib/tubular.module.spec.ts
+++ lib/tubular.module.spec.ts
@@ -1,5 +1,44 @@
-import {TubularModule} from './tubular.module';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TubularModule } from './tubular.module';
+import { MDatePipe } from './mdate.pipe';
 
 describe('module: tubular', () => {
-    it('should pass', () => { });
+    let tubularModule: TubularModule;
+    let fixture: ComponentFixture<TubularModule>;
+
+    beforeEach(() => {
+        //tubularModule = new TubularModule();
+        TestBed.configureTestingModule({
+            declarations: [TubularModule],
+        });
+        
+        fixture = TestBed.createComponent(TubularModule);
+        tubularModule = fixture.componentInstance;
+    });     
+     
+    it('should success', () => {
+        expect(true).toBe(true);
+    });
+    it('should be defined', () => {
+        expect(tubularModule).toBeDefined();
+    });
+});
+
+describe('Mdate pipe Component: MDataType ',function(){
+    let mDate: MDatePipe;
+    let fixture: ComponentFixture<MDatePipe>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [MDatePipe]
+        });
+        fixture = TestBed.createComponent(MDatePipe);
+        mDate = fixture.componentInstance;
+    });  
+
+    it('should have a defined component', () => {
+        expect(mDate).toBeDefined();
+    });
+
 });
