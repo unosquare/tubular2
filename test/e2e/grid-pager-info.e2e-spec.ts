da9bc73739961361a7167283f97b216b5dc5c195
alexey.turlapov <alexey.turlapov@unosquare.com>
2017-04-06 14:53:46 -0500

    adding chai & chai-as-promised

--- test/e2e/grid-pager-info.e2e-spec.ts
+++ test/e2e/grid-pager-info.e2e-spec.ts
@@ -1,5 +1,11 @@
 ﻿import { browser, element, by } from '../../node_modules/protractor/built';
 
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+var expect = chai.use(chaiAsPromised).expect;
+var should = chai.use(chaiAsPromised).should();
+
+
 describe('grid pager info', () => {
 
     let paginator = element(by.tagName('ngb-pagination')).$$('li'),
@@ -13,16 +19,16 @@ describe('grid pager info', () => {
     });
 
     it('should show text in accordance to numbered of filter rows and current results-page',() => {
-        expect(gridPagerInfo.first().getText()).toEqual('Showing 1 to 10 of 500 records');
+        expect(gridPagerInfo.first().getText()).eventually.be('Showing 1 to 10 of 500 records');
         paginator.get(7).$$('a').click();
         pageSizeSelector.$('[value="20"]').click();
-        expect(gridPagerInfo.first().getText()).toEqual('Showing 21 to 40 of 500 records');
+        expect(gridPagerInfo.first().getText()).eventually.be('Showing 21 to 40 of 500 records');
         paginator.get(5).$$('a').click();
-        expect(gridPagerInfo.first().getText()).toEqual('Showing 61 to 80 of 500 records');
+        expect(gridPagerInfo.first().getText()).eventually.be('Showing 61 to 80 of 500 records');
     });
 
     it('should show count in footer', () => {
         gridSearchInput.sendKeys('a');
-        expect(gridPagerInfo.first().getText()).toContain('Showing 1 to 10 of 454 records (Filtered from 500 total records)');
+        expect(gridPagerInfo.first().getText()).eventually.contain('Showing 1 to 10 of 454 records (Filtered from 500 total records)');
     });
 });
