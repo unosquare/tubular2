3702f2a420fb803b44d0f348aa7ce870908b46cc
alexey.turlapov <alexey.turlapov@unosquare.com>
2017-04-07 09:11:51 -0500

    fixing mdate test

--- lib/mdate.pipe.spec.ts
+++ lib/mdate.pipe.spec.ts
@@ -2,23 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MDatePipe } from './mdate.pipe';
 
-describe('Component: MDatePipe', () => {
-    let mDate: MDatePipe;
-    let fixture: ComponentFixture<MDatePipe>;
+describe('Pipe: MDatePipe', () => {
+    let pipe: MDatePipe;
 
-    beforeEach(() => {
-
+    beforeEach(()  => {
+        pipe = new MDatePipe();
     });
 
-    it('should have a defined component', async () => {
-        TestBed.configureTestingModule({
-            declarations: [MDatePipe]
-        }).compileComponents().then(() => {
-            fixture = TestBed.createComponent(MDatePipe);
-            mDate = fixture.componentInstance;
-            expect(mDate).toBeDefined();
-        })
-
-
+    it('should return the same value if not date', () => {
+        expect(pipe.transform('abc')).toBe('abc');
     });
+
 });
