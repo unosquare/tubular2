4f7e1b55ee8823cc2423cd1ff27f81f61809f636
Kadosh Alejandro <kadosh.ivan@gmail.com>
2017-08-31 15:00:21 -0500

    Trying to integrate with webpack

--- karma.conf.js
+++ karma.conf.js
@@ -1,119 +1,26 @@
 module.exports = function (config) {
 
-  var libBase = 'src/lib/';       // transpiled app JS and map files
-
   config.set({
-    basePath: '',
-    frameworks: ['jasmine'],
-
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-html-reporter'),
-      require('karma-coverage'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter')
-    ],
-
-    client: {
-      builtPaths: [libBase], // add more spec base paths as needed
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-
-    customLaunchers: {
-      // From the CLI. Not used here but interesting
-      // chrome setup for travis CI using chromium
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
-
-    files: [
-      // System.js for module loading
-      'node_modules/systemjs/dist/system.src.js',
-
-      // Polyfills
-      'node_modules/core-js/client/shim.js',
-
-      // zone.js
-      'node_modules/zone.js/dist/zone.js',
-      'node_modules/zone.js/dist/long-stack-trace-zone.js',
-      'node_modules/zone.js/dist/proxy.js',
-      'node_modules/zone.js/dist/sync-test.js',
-      'node_modules/zone.js/dist/jasmine-patch.js',
-      'node_modules/zone.js/dist/async-test.js',
-      'node_modules/zone.js/dist/fake-async-test.js',
-      
-      'node_modules/moment/moment.js',
-
-      // RxJs
-      { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
-      { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
-
-      // Paths loaded via module imports:
-      // Angular itself
-      { pattern: 'node_modules/@angular/**/*.js', included: false, watched: false },
-      { pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false },
-
-      { pattern: 'src/demo/systemjs-angular-loader.js', included: false, watched: false },
-
-      'karma-test-shim.js', // optionally extend SystemJS mapping e.g., with barrels
-
-      // transpiled application & spec code paths loaded via module imports
-      { pattern: libBase + '**/*.js', included: false, watched: true },
-
-      // Asset (HTML & CSS) paths loaded via Angular's component compiler
-      // (these paths need to be rewritten, see proxies section)
-      { pattern: libBase + '**/*.html', included: false, watched: true },
-      { pattern: libBase + '**/*.css', included: false, watched: true },
-
-      // Paths for debugging with source maps in dev tools
-      { pattern: libBase + '**/*.ts', included: false, watched: false },
-      { pattern: libBase + '**/*.js.map', included: false, watched: false }
-    ],
-
-    // Proxied base paths for loading assets
-    proxies: {
-      // required for modules fetched by SystemJS
-      '/base/src/lib/node_modules/': '/base/node_modules/',
-      '/base/src/lib/demo/': '/base/src/demo/',
-      '/base/src/lib/moment.js': '/base/node_modules/moment/moment.js'
-    },
-
-    exclude: [],
-
-    preprocessors: {
-        [libBase + '/**/!(*spec|*bundle).js'] : ['coverage']
-    },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage', 'kjhtml', 'html'],
-
-    htmlReporter: {
-        outputDir: 'report/unit', // where to put the reports
-        focusOnFailures: true, // reports show failures on start
-        namedFiles: true, // name files instead of creating sub-directories
-        reportName: 'index',
-
-        // experimental
-        preserveDescribeNesting: false, // folded suites stay folded
-        foldAll: false // reports start folded (only with preserveDescribeNesting)
-    },
-
-    // optionally, configure the reporter
-    coverageReporter: {
-        type: 'lcov',
-        dir: 'report/coverage',
-        subdir: '.'
-    },
-
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
-  })
+    files: [
+      './node_modules/es6-shim/es6-shim.min.js',
+      'karma.entry.js',
+    ],
+    frameworks: ['jasmine'],
+    logLevel: config.LOG_INFO,
+    phantomJsLauncher: {
+      exitOnResourceError: true
+    },
+    port: 9876,
+    preprocessors: {
+      'karma.entry.js': ['webpack', 'sourcemap']
+    },
+    reporters: ['dots'],
+    singleRun: true,
+    webpack: require('./webpack.test.js'),
+    webpackServer: {
+      noInfo: true
+    }
+  });
 }
