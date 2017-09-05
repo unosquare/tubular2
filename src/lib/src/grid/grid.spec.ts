8c576d2358630f16fe57d2a2676ae1fb18a57d8d
Kadosh Alejandro <kadosh.ivan@gmail.com>
2017-09-05 10:43:00 -0500

    Fixed async references on grid.spec.

--- src/lib/src/grid/grid.spec.ts
+++ src/lib/src/grid/grid.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, getTestBed, inject, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed, getTestBed, inject, tick, async } from '@angular/core/testing';
 
 import { MockBackend, MockConnection } from '@angular/http/testing';
 import { BaseRequestOptions, Http, HttpModule, ResponseOptions, Response } from '@angular/http';
@@ -33,7 +33,7 @@ describe('Component: GridComponent', () => {
             [483, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
             [482, 'Super La Playa', '2016-11-07T00:00:00', '2016-01-01T00:00:00', 'Leon, GTO, Mexico'],
             [481, 'Oxxo', '2015-02-24T12:00:00', '2015-12-30T00:00:00', 'Guadalajara, JAL, Mexico']
-            ],
+        ],
         TotalRecordCount: 500,
         FilteredRecordCount: 500,
         TotalPages: 25,
@@ -44,25 +44,25 @@ describe('Component: GridComponent', () => {
         Counter: 1,
         Payload: [[
             1, 'Microsoft', '2017-02-01T23:00:00', '2015-12-30T00:00:00', 'Guadalajara, JAL, Mexico'],
-            [3, 'Unosquare LLC', '2016-12-18T00:00:00', '2016-01-01T00:00:00', 'Portland, OR, USA'],
-            [11, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
-            [7, 'Microsoft', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Leon, GTO, Mexico'],
-            [21, 'Unosquare LLC', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Leon, GTO, Mexico'],
-            [31, 'Super La Playa', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Leon, GTO, Mexico'],
-            [36, 'Super La Playa', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Leon, GTO, Mexico'],
-            [45, 'Unosquare LLC', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
-            [48, 'Unosquare LLC', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Leon, GTO, Mexico'],
-            [51, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Portland, OR, USA'],
-            [59, 'Microsoft', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Los Angeles, CA, USA'],
-            [63, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
-            [72, 'Microsoft', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Guadalajara, JAL, Mexico'],
-            [95, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
-            [127, 'Microsoft', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Portland, OR, USA'],
-            [130, 'Super La Playa', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Los Angeles, CA, USA'],
-            [137, 'Unosquare LLC', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Guadalajara, JAL, Mexico'],
-            [141, 'Vesta', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Los Angeles, CA, USA'],
-            [144, 'Oxxo', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Leon, GTO, Mexico'],
-            [145, 'Super La Playa', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Guadalajara, JAL, Mexico']
+        [3, 'Unosquare LLC', '2016-12-18T00:00:00', '2016-01-01T00:00:00', 'Portland, OR, USA'],
+        [11, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
+        [7, 'Microsoft', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Leon, GTO, Mexico'],
+        [21, 'Unosquare LLC', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Leon, GTO, Mexico'],
+        [31, 'Super La Playa', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Leon, GTO, Mexico'],
+        [36, 'Super La Playa', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Leon, GTO, Mexico'],
+        [45, 'Unosquare LLC', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
+        [48, 'Unosquare LLC', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Leon, GTO, Mexico'],
+        [51, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Portland, OR, USA'],
+        [59, 'Microsoft', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Los Angeles, CA, USA'],
+        [63, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
+        [72, 'Microsoft', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Guadalajara, JAL, Mexico'],
+        [95, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
+        [127, 'Microsoft', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Portland, OR, USA'],
+        [130, 'Super La Playa', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Los Angeles, CA, USA'],
+        [137, 'Unosquare LLC', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Guadalajara, JAL, Mexico'],
+        [141, 'Vesta', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Los Angeles, CA, USA'],
+        [144, 'Oxxo', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Leon, GTO, Mexico'],
+        [145, 'Super La Playa', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Guadalajara, JAL, Mexico']
         ],
         TotalRecordCount: 500,
         FilteredRecordCount: 500,
@@ -72,7 +72,7 @@ describe('Component: GridComponent', () => {
     };
     let response: ResponseOptions;
 
-    beforeEach(async () => {
+    beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [GridComponent],
             providers:
@@ -86,15 +86,13 @@ describe('Component: GridComponent', () => {
                 }
             ],
             imports: [HttpModule]
-        });
-    });
+        }).compileComponents();
+    }));
 
     it('should be defined', () => {
-        TestBed.compileComponents().then(() => {
-            fixture = TestBed.createComponent(GridComponent);
-            gridComponent = fixture.componentInstance;
-            expect(gridComponent).toBeDefined();
-        });
+        fixture = TestBed.createComponent(GridComponent);
+        gridComponent = fixture.componentInstance;
+        expect(gridComponent).toBeDefined();
     });
 
     describe('MockBackend', () => {
