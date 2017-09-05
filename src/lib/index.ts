136cc9a6a30bbb09be207860b7411172471cc316
Kadosh Alejandro <kadosh.ivan@gmail.com>
2017-09-05 09:57:32 -0500

    Re-oganization on lib folder to match library seed

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,16 +4,16 @@
  * Entry point for all public APIs of Tubular2.
  */
 
-export * from './core';
-export * from './module';
+export * from './src/core';
+export * from './src/module';
 
-export * from './column-filter-dialog/index';
-export * from './grid/index';
-export * from './grid-export/grid-export';
-export * from './grid-page-size-selector/grid-page-size-selector';
-export * from './grid-pager/grid-pager';
-export * from './grid-pager-info/grid-pager-info';
-export * from './grid-print/grid-print';
-export * from './grid-search/grid-search';
-export * from './grid-table/grid-table';
-export * from './popover/index';
+export * from './src/column-filter-dialog/index';
+export * from './src/grid/index';
+export * from './src/grid-export/grid-export';
+export * from './src/grid-page-size-selector/grid-page-size-selector';
+export * from './src/grid-pager/grid-pager';
+export * from './src/grid-pager-info/grid-pager-info';
+export * from './src/grid-print/grid-print';
+export * from './src/grid-search/grid-search';
+export * from './src/grid-table/grid-table';
+export * from './src/popover/index';
