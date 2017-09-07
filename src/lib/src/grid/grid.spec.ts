0a61f04e1c538e5a45ceba26432e7c0cca9bb022
Kadosh Alejandro <kadosh.ivan@gmail.com>
2017-09-07 10:26:11 -0500

    Added initial placeholder for grid-spec

--- src/lib/src/grid/grid.spec.ts
+++ src/lib/src/grid/grid.spec.ts
@@ -1,122 +1,133 @@
 import { ComponentFixture, TestBed, getTestBed, inject, tick, async } from '@angular/core/testing';
+import { Component, ViewChild } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { MaterialModule } from '@angular/material';
 
 import { MockBackend, MockConnection } from '@angular/http/testing';
 import { BaseRequestOptions, Http, HttpModule, ResponseOptions, Response } from '@angular/http';
 
 import { GridComponent } from './grid';
+import { GridPrintButtonDirective } from '../grid-print/grid-print';
+import { GridExportButtonDirective } from '../grid-export/grid-export';
+import { TubularDataSource } from '../grid-table/grid-table';
 
 describe('Component: GridComponent', () => {
-    let gridComponent: GridComponent;
-    let fixture: ComponentFixture<GridComponent>;
-    let mockBackend: MockBackend;
-    const mockUrl = '/mock/api';
-    const mockJson = {
-        Counter: 0,
-        Payload: [
-            [500, 'Vesta', '2016-11-03T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
-            [499, 'Oxxo', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Portland, OR, USA'],
-            [498, 'Unosquare LLC', '2016-11-09T00:00:00', '2016-01-01T00:00:00', 'Portland, OR, USA'],
-            [497, 'Microsoft', '2016-11-04T00:00:00', '2016-01-01T00:00:00', 'Los Angeles, CA, USA'],
-            [496, 'Vesta', '2016-11-06T00:00:00', '2016-01-01T00:00:00', 'Guadalajara, JAL, Mexico'],
-            [495, 'Oxxo', '2016-11-11T00:00:00', '2015-12-30T00:00:00', 'Guadalajara, JAL, Mexico'],
-            [494, 'Vesta', '2016-11-04T00:00:00', '2015-12-30T00:00:00', 'Portland, OR, USA'],
-            [493, 'Vesta', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Leon, GTO, Mexico'],
-            [492, 'Oxxo', '2016-11-10T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
-            [491, 'Unosquare LLC', '2016-11-03T00:00:00', '2016-01-01T00:00:00', 'Los Angeles, CA, USA'],
-            [490, 'Vesta', '2016-11-06T00:00:00', '2015-12-30T00:00:00', 'Guadalajara, JAL, Mexico'],
-            [489, 'Unosquare LLC', '2016-11-11T00:00:00', '2016-01-01T00:00:00', 'Portland, OR, USA'],
-            [488, 'Microsoft', '2016-11-03T00:00:00', '2016-01-01T00:00:00', 'Guadalajara, JAL, Mexico'],
-            [487, 'Unosquare LLC', '2016-11-06T00:00:00', '2016-01-01T00:00:00', 'Portland, OR, USA'],
-            [486, 'Oxxo', '2016-11-11T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
-            [485, 'Advanced Technology Systems', '2016-11-05T00:00:00', '2016-01-01T00:00:00', 'Portland, OR, USA'],
-            [484, 'Advanced Technology Systems', '2016-11-04T00:00:00', '2016-01-01T00:00:00', 'Leon, GTO, Mexico'],
-            [483, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
-            [482, 'Super La Playa', '2016-11-07T00:00:00', '2016-01-01T00:00:00', 'Leon, GTO, Mexico'],
-            [481, 'Oxxo', '2015-02-24T12:00:00', '2015-12-30T00:00:00', 'Guadalajara, JAL, Mexico']
-        ],
-        TotalRecordCount: 500,
-        FilteredRecordCount: 500,
-        TotalPages: 25,
-        CurrentPage: 1,
-        AggregationPayload: {}
-    };
-    const mockJsonTest = {
-        Counter: 1,
-        Payload: [[
-            1, 'Microsoft', '2017-02-01T23:00:00', '2015-12-30T00:00:00', 'Guadalajara, JAL, Mexico'],
-        [3, 'Unosquare LLC', '2016-12-18T00:00:00', '2016-01-01T00:00:00', 'Portland, OR, USA'],
-        [11, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
-        [7, 'Microsoft', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Leon, GTO, Mexico'],
-        [21, 'Unosquare LLC', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Leon, GTO, Mexico'],
-        [31, 'Super La Playa', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Leon, GTO, Mexico'],
-        [36, 'Super La Playa', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Leon, GTO, Mexico'],
-        [45, 'Unosquare LLC', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
-        [48, 'Unosquare LLC', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Leon, GTO, Mexico'],
-        [51, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Portland, OR, USA'],
-        [59, 'Microsoft', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Los Angeles, CA, USA'],
-        [63, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
-        [72, 'Microsoft', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Guadalajara, JAL, Mexico'],
-        [95, 'Vesta', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Los Angeles, CA, USA'],
-        [127, 'Microsoft', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Portland, OR, USA'],
-        [130, 'Super La Playa', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Los Angeles, CA, USA'],
-        [137, 'Unosquare LLC', '2016-11-12T00:00:00', '2015-12-30T00:00:00', 'Guadalajara, JAL, Mexico'],
-        [141, 'Vesta', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Los Angeles, CA, USA'],
-        [144, 'Oxxo', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Leon, GTO, Mexico'],
-        [145, 'Super La Playa', '2016-11-12T00:00:00', '2016-01-01T00:00:00', 'Guadalajara, JAL, Mexico']
-        ],
-        TotalRecordCount: 500,
-        FilteredRecordCount: 500,
-        TotalPages: 25,
-        CurrentPage: 3,
-        AggregationPayload: {}
-    };
-    let response: ResponseOptions;
+    let simpleGridApp: SimpleGridApp;
+    let fixture: ComponentFixture<SimpleGridApp>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [GridComponent],
-            providers:
-            [
-                MockBackend,
-                BaseRequestOptions,
-                {
-                    provide: Http,
-                    deps: [MockBackend, BaseRequestOptions],
-                    useFactory: (backend: MockBackend, options: BaseRequestOptions) => { new Http(backend, options); }
-                }
-            ],
-            imports: [HttpModule]
+            declarations: [SimpleGridApp, GridComponent, GridPrintButtonDirective, GridExportButtonDirective],
+            imports: [MaterialModule, HttpModule]
         }).compileComponents();
     }));
 
-    it('should be defined', () => {
-        fixture = TestBed.createComponent(GridComponent);
-        gridComponent = fixture.componentInstance;
-        expect(gridComponent).toBeDefined();
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SimpleGridApp);
+        fixture.detectChanges();
+        fixture.detectChanges();
     });
 
-    describe('MockBackend', () => {
+    it('should instantiate grid', () => {
+        const progressBar = fixture.nativeElement.querySelector('.tb-progress-bar');
+        expect(progressBar).toBeDefined();
+        // const tableElement = fixture.nativeElement.querySelector('.tb-progress-bar');
+        // const headerRow = tableElement.querySelectorAll('.mat-header-cell')
 
-        beforeEach(() => {
-            gridComponent.dataUrl = '/mock/api'; // http://0.0.0.0:9876/
-            // MockedBackend
-            mockBackend = getTestBed().get(MockBackend);
-            getTestBed().compileComponents().then(() => {
-                mockBackend.connections.subscribe((c: MockConnection) => {
-                    if (c.request.url === '/mock/api') {
-                        response = new ResponseOptions({ body: JSON.stringify(mockJson) });
-                        c.mockRespond(new Response(response));
-                    }
-                });
-            });
-        });
-
-        xit('should get data', (done) => {
-            expect(gridComponent.dataUrl).toBeDefined();
-            // gridComponent((response) => {
-            //     expect(response).toBeDefined();
-            //     done();
-            // });
-        });
+        // const rows = tableElement.querySelectorAll('.mat-row');
+        // for (let i = 0; i < rows.length; i++) {
+        //     const cells = rows[i].querySelectorAll('.mat-cell');
+        //     expectTextContent(cells[0], `a_${i + 1}`);
+        //     expectTextContent(cells[1], `b_${i + 1}`);
+        //     expectTextContent(cells[2], `c_${i + 1}`);
+        // }
     });
 });
+
+interface TestData {
+    a: string;
+    b: string;
+    c: string;
+}
+
+class FakeDataSource extends DataSource<TestData> {
+    _dataChange = new BehaviorSubject<TestData[]>([]);
+    set data(data: TestData[]) { this._dataChange.next(data); }
+    get data() { return this._dataChange.getValue(); }
+
+    constructor() {
+        super();
+        for (let i = 0; i < 3; i++) { this.addData(); }
+    }
+
+    connect(): Observable<TestData[]> {
+        return this._dataChange;
+    }
+
+    disconnect() { }
+
+    addData() {
+        const nextIndex = this.data.length + 1;
+
+        let copiedData = this.data.slice();
+        copiedData.push({
+            a: `a_${nextIndex}`,
+            b: `b_${nextIndex}`,
+            c: `c_${nextIndex}`
+        });
+
+        this.data = copiedData;
+    }
+}
+
+@Component({
+    template: `
+    <tb-grid #grid dataUrl="http://tubular.azurewebsites.net/api/orders/paged">
+        <md-toolbar>
+            <div fxFlex>
+                
+            </div>
+            <div fxFlex>
+            <button md-icon-button (click)="add()" aria-label="Add New">
+                <md-icon>add</md-icon>
+            </button>
+                <button md-icon-button [grid-print]="grid" aria-label="Print">
+                <md-icon>print</md-icon>
+            </button>
+            <button md-icon-button [grid-export]="grid" file-name="sampleCsv.csv" aria-label="Export">
+                <md-icon>file_download</md-icon>
+            </button>
+            </div>
+            <div fxFlex>
+                
+            </div>
+        </md-toolbar>
+        <div fxLayout="row" >
+            <md-progress-bar [color]="primary" mode="indeterminate" *ngIf="grid.isLoading" class="tb-progress-bar">
+            </md-progress-bar>
+        </div>
+        <div fxLayout="row">
+            
+        </div>
+        <md-toolbar>
+            <div fxFlex>
+                
+            </div>
+            <div fxFlex>
+                
+            </div>
+            <div fxFlex>
+                
+            </div>
+        </md-toolbar>
+    </tb-grid>
+    `
+})
+class SimpleGridApp {
+    @ViewChild(GridComponent) table: GridComponent;
+
+    dataSource: TubularDataSource | null = new TubularDataSource(this.table);
+    columnsToRender = ['column_a', 'column_b', 'column_c'];
+}
