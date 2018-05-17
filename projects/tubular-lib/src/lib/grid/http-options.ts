e079e9893e1260024ac4a6fb53d1f8aa108c36a4
Geovanni Perez <geovanni.perez@gmail.com>
2018-05-17 15:38:22 -0500

    Delete http-options.ts

--- projects/tubular-lib/src/lib/grid/http-options.ts
+++
@@ -1,6 +0,0 @@
-import { HttpHeaders } from '@angular/common/http';
-
-export class HttpOptions {
-    // The is try to use Injector
-    headers: Headers = new Headers({ Authorization: JSON.parse(localStorage.getItem('auth_Header'))});
-}
