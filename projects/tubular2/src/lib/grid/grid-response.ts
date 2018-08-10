68e1d43150f5ba6125c6bab9ae96f1a0daef7a3c
Geo Perez <geovanni.perez@gmail.com>
2018-08-10 14:22:55 -0500

    Remove more models

--- projects/tubular2/src/lib/grid/grid-response.ts
+++
@@ -1,9 +0,0 @@
-export interface GridResponse {
-    Counter: number;
-    Payload: AnalyserNode[];
-    TotalRecordCount: number;
-    FilteredRecordCount: number;
-    TotalPages: number;
-    CurrentPage: number;
-    AggregationPayload: any;
-}
