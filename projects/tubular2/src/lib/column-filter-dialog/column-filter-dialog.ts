b58ea16f90d002bc59e1e5e43f4f9128082b1997
Geo Perez <geovanni.perez@gmail.com>
2018-08-10 15:09:17 -0500

    Remove popover

--- projects/tubular2/src/lib/column-filter-dialog/column-filter-dialog.ts
+++ projects/tubular2/src/lib/column-filter-dialog/column-filter-dialog.ts
@@ -12,7 +12,6 @@
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { ColumnModel } from 'tubular-common';
 import { GridComponent } from '../grid/grid';
-import { NgbPopover } from '../popover/popover';
 import { MAT_CHECKBOX_CLICK_ACTION } from '@angular/material';
 
 @Component({
@@ -21,11 +20,6 @@ import { MAT_CHECKBOX_CLICK_ACTION } from '@angular/material';
     styleUrls: ['./column-filter-dialog.css']
 })
 export class ColumnFilterDialogComponent implements AfterViewInit, OnInit {
-    @ContentChild('filterPopover')
-    public filterPopoverTemplate: TemplateRef<Object>;
-
-    @ViewChild('popover') private popover: NgbPopover;
-
     @Output() toggleEvent: EventEmitter<any> = new EventEmitter();
 
     @Input()
@@ -68,7 +62,6 @@ export class ColumnFilterDialogComponent implements AfterViewInit, OnInit {
 
     public submit() {
         this.tbGrid.filterByColumnName(this.column);
-        this.popover.close();
     }
 
     public reset() {
@@ -76,15 +69,6 @@ export class ColumnFilterDialogComponent implements AfterViewInit, OnInit {
         this.columnModel.Filter = null;
 
         this.tbGrid.filterByColumnName(this.column);
-    }
-
-    public cancel() {
-        this.form.reset();
-        this.columnModel.filter.argument = null;
-        this.columnModel.filter.operator = 'None';
-
-        this.tbGrid.filterByColumnName(this.column);
-        this.popover.close();
     }
 
     public selectChange(newVal: any) {
@@ -98,25 +82,15 @@ export class ColumnFilterDialogComponent implements AfterViewInit, OnInit {
     public ngAfterViewInit() {
         // set initial value in form with a timeout
         setTimeout(_ => {
-
             // set initial value in form with a timeout
             this.form.patchValue({
                 filter: this.columnModel.Filter
             });
-
         });
     }
 
     public toggleClick() {
-        this.isDialogOpen = !this.isDialogOpen;
-        this.togglePopover();
-    }
-
-    public togglePopover() {
-        if (this.isDialogOpen) {
-            this.popover.open();
-        } else {
-            this.popover.close();
-        }
+        // TODO: Change to Modal for now
+        // https://material.angular.io/components/dialog/overview
     }
 }
