f64d41642af68b05653de70a667a0d25d1871aba
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-02-25 13:42:39 -0600

    Again

--- docs/runtime.js
+++ docs/runtime.js
@@ -136,7 +136,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "https://unosquare.github.io/tubular2";
+/******/ 	__webpack_require__.p = "https://unosquare.github.io/tubular2/";
 /******/
 /******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
 /******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
